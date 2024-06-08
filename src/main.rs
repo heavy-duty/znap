8be6f3b0866394c94ccc634133b0832a5a2a51ef
Dan <danielarturomt@gmail.com>
2024-06-08 14:09:55 +0100

    refactor: improve logic

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,16 @@ use std::str::FromStr;
 
 pub use self::error::{Error, Result};
 
-use axum::{extract::{Path, Query}, routing::get, Json, Router};
+use axum::{
+    extract::Query,
+    routing::get,
+    Json, Router,
+};
+use base64::prelude::*;
 use bincode::serialize;
 use serde::{Deserialize, Serialize};
 use solana_sdk::{message::Message, pubkey::Pubkey, transaction::Transaction};
 use tokio::net::TcpListener;
-use base64::prelude::*;
 
 mod error;
 
@@ -29,59 +33,17 @@ async fn main() -> Result<()> {
 // region:    --- Routes Hello
 fn routes_actions() -> Router {
     Router::new().route(
-        "/actions/:action_id",
-        get(handle_get_action).post(handle_create_action),
+        "/actions/fixed_transfer",
+        get(handle_get_fixed_transfer).post(handle_create_fixed_transfer),
     )
 }
 
-#[derive(Debug, Deserialize)]
-struct GetActionParams {
-    action_id: u32,
-}
-
 #[derive(Debug, Serialize)]
-struct GetFixedTransferResponse {
+struct GetActionResponse {
     icon: String,
     title: String,
     description: String,
     label: String,
-}
-
-// e.g., `/actions/:actionId`
-async fn handle_get_action(
-    Path(params): Path<GetActionParams>,
-) -> Result<Json<GetFixedTransferResponse>> {
-    println!("->> {:<12} - get_action - {params:?}", "HANDLER");
-
-    let response = match params.action_id {
-        0 => {
-            let icon = String::from("https://url.com");
-            let title = String::from("Send a fixed transfer");
-            let description = String::from("This action allows you to send a fixed transfer");
-            let label = String::from("Send");
-
-            GetFixedTransferResponse {
-                icon,
-                title,
-                description,
-                label,
-            }
-        }
-        _ => return Err(Error::ActionNotFound),
-    };
-
-    Ok(Json(response))
-}
-
-#[derive(Debug, Deserialize)]
-struct CreateActionParams {
-    action_id: u32,
-}
-
-
-#[derive(Debug, Deserialize)]
-struct CreateActionQuery {
-    amount: u64,
 }
 
 #[derive(Debug, Deserialize)]
@@ -90,65 +52,102 @@ struct CreateActionPayload {
 }
 
 #[derive(Debug, Serialize)]
-struct CreateFixedTransferResponse {
+struct CreateActionResponse {
     transaction: String,
     message: Option<String>,
 }
 
-// e.g., `/actions/:actionId`
-async fn handle_create_action(
-    Query(query): Query<CreateActionQuery>,
-    Path(params): Path<CreateActionParams>,
-    Json(payload): Json<CreateActionPayload>,
-) -> Result<Json<CreateFixedTransferResponse>> {
-    println!(
-        "->> {:<12} - create_action - {params:?} - {payload:?}",
-        "HANDLER"
-    );
+#[derive(Debug, Deserialize)]
+struct CreateFixedTransferQuery {
+    amount: u64,
+}
 
-    let account_pubkey = match Pubkey::from_str(&payload.account) {
-        Ok(account_pubkey) => account_pubkey,
-        _ => return Err(Error::InvalidAccountPubkey)
-    };
-    
-    let response = match params.action_id {
-        0 => {
-            let mint_pubkey = Pubkey::from_str(&"4PYnraBJbdPXeMXdgL5k1m3TCcfNMaEWycvEQu2cteEV").unwrap();
-            let receiver_pubkey = Pubkey::from_str(&"6GBLiSwAPhDMttmdjo3wvEsssEnCiW3yZwVyVZnhFm3G").unwrap();
-            let source_pubkey = spl_associated_token_account::get_associated_token_address(
-                &account_pubkey,
-                &mint_pubkey,
-            );
-            let destination_pubkey = spl_associated_token_account::get_associated_token_address(
-                &receiver_pubkey,
-                &mint_pubkey,
-            );
-            let transfer_instruction = match spl_token::instruction::transfer(
-                &spl_token::ID,
-                &source_pubkey,
-                &destination_pubkey,
-                &account_pubkey,
-                &[&account_pubkey],
-                query.amount,
-            ) {
-                Ok(transfer_instruction) => transfer_instruction,
-                _ => return Err(Error::InvalidTransferInstruction)
-            };
-            let transaction_message = Message::new(&[transfer_instruction], None);
-            let transaction: Transaction = Transaction::new_unsigned(transaction_message);
-            let serialized_transaction = match serialize(&transaction) {
-                Ok(serialized_transaction) => serialized_transaction,
-                _ => return Err(Error::InvalidTransferInstruction)
-            };
-            let encoded_transaction = BASE64_STANDARD.encode(serialized_transaction);
-            
-            CreateFixedTransferResponse {
-                transaction: encoded_transaction,
-                message: None,
-            }
-        }
-        _ => return Err(Error::ActionNotFound),
+// e.g., `/actions/fixed_transfer`
+async fn handle_get_fixed_transfer() -> Result<Json<GetActionResponse>> {
+    println!("->> get_fixed_transfer");
+
+    let icon = String::from("https://url.com");
+    let title = String::from("Send a fixed transfer");
+    let description = String::from("This action allows you to send a fixed transfer");
+    let label = String::from("Send");
+
+    let response = GetActionResponse {
+        icon,
+        title,
+        description,
+        label,
     };
 
     Ok(Json(response))
+}
+
+// e.g., `/actions/fixed_transfer`
+async fn handle_create_fixed_transfer(
+    Query(query): Query<CreateFixedTransferQuery>,
+    Json(payload): Json<CreateActionPayload>,
+) -> Result<Json<CreateActionResponse>> {
+    println!("->> - create_fixed_transfer - {payload:?}");
+
+    let fixed_transfer_action = FixedTransferAction::new(query.amount);
+    let response = fixed_transfer_action.create_transaction(payload)?;
+
+    Ok(Json(response))
+}
+
+trait CreateTransaction {
+    fn create_transaction(&self, payload: CreateActionPayload) -> Result<CreateActionResponse>;
+}
+
+struct FixedTransferAction {
+    amount: u64,
+}
+
+impl FixedTransferAction {
+    fn new(amount: u64) -> Self {
+        Self { amount }
+    }
+}
+
+impl CreateTransaction for FixedTransferAction {
+    fn create_transaction(&self, payload: CreateActionPayload) -> Result<CreateActionResponse> {
+        let account_pubkey = match Pubkey::from_str(&payload.account) {
+            Ok(account_pubkey) => account_pubkey,
+            _ => return Err(Error::InvalidAccountPubkey),
+        };
+        let mint_pubkey =
+            Pubkey::from_str(&"4PYnraBJbdPXeMXdgL5k1m3TCcfNMaEWycvEQu2cteEV").unwrap();
+        let receiver_pubkey =
+            Pubkey::from_str(&"6GBLiSwAPhDMttmdjo3wvEsssEnCiW3yZwVyVZnhFm3G").unwrap();
+        let source_pubkey = spl_associated_token_account::get_associated_token_address(
+            &account_pubkey,
+            &mint_pubkey,
+        );
+        let destination_pubkey = spl_associated_token_account::get_associated_token_address(
+            &receiver_pubkey,
+            &mint_pubkey,
+        );
+        let transfer_instruction = match spl_token::instruction::transfer(
+            &spl_token::ID,
+            &source_pubkey,
+            &destination_pubkey,
+            &account_pubkey,
+            &[&account_pubkey],
+            self.amount,
+        ) {
+            Ok(transfer_instruction) => transfer_instruction,
+            _ => return Err(Error::InvalidTransferInstruction),
+        };
+        let transaction_message = Message::new(&[transfer_instruction], None);
+        let transaction: Transaction = Transaction::new_unsigned(transaction_message);
+        let serialized_transaction = match serialize(&transaction) {
+            Ok(serialized_transaction) => serialized_transaction,
+            _ => return Err(Error::InvalidTransferInstruction),
+        };
+        let encoded_transaction = BASE64_STANDARD.encode(serialized_transaction);
+
+        Ok(CreateActionResponse {
+            transaction: encoded_transaction,
+            message: None,
+        })
+    }
 }
