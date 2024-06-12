1fa27e651ed8675f408ec7215d94274bb2290c3d
Dan <danielarturomt@gmail.com>
2024-06-12 18:40:57 +0100

    chore: create a dynamic transfer action

--- my-actions/src/lib.rs
+++ my-actions/src/lib.rs
@@ -57,7 +57,46 @@ pub trait HandlePostAction {
 pub mod my_actions {
     use super::*;
 
-    pub fn fixed_transfer2(ctx: Context<FixedTransferAction>) -> Result<String, Error> {
+    pub fn fixed_transfer(ctx: Context<FixedTransferAction>) -> Result<String, Error> {
+        let account_pubkey = match Pubkey::from_str(&ctx.payload.account) {
+            Ok(account_pubkey) => account_pubkey,
+            _ => return Err(Error::InvalidAccountPubkey),
+        };
+        let mint_pubkey =
+            Pubkey::from_str(&"4PYnraBJbdPXeMXdgL5k1m3TCcfNMaEWycvEQu2cteEV").unwrap();
+        let receiver_pubkey =
+            Pubkey::from_str(&"6GBLiSwAPhDMttmdjo3wvEsssEnCiW3yZwVyVZnhFm3G").unwrap();
+        let source_pubkey = spl_associated_token_account::get_associated_token_address(
+            &account_pubkey,
+            &mint_pubkey,
+        );
+        let destination_pubkey = spl_associated_token_account::get_associated_token_address(
+            &receiver_pubkey,
+            &mint_pubkey,
+        );
+        let transfer_instruction = match spl_token::instruction::transfer(
+            &spl_token::ID,
+            &source_pubkey,
+            &destination_pubkey,
+            &account_pubkey,
+            &[&account_pubkey],
+            1,
+        ) {
+            Ok(transfer_instruction) => transfer_instruction,
+            _ => return Err(Error::InvalidInstruction),
+        };
+        let transaction_message = Message::new(&[transfer_instruction], None);
+        let transaction = Transaction::new_unsigned(transaction_message);
+        let serialized_transaction = match serialize(&transaction) {
+            Ok(serialized_transaction) => serialized_transaction,
+            _ => return Err(Error::InvalidInstruction),
+        };
+        let encoded_transaction = BASE64_STANDARD.encode(serialized_transaction);
+
+        Ok(encoded_transaction)
+    }
+
+    pub fn dynamic_transfer(ctx: Context<DynamicTransferAction>) -> Result<String, Error> {
         let account_pubkey = match Pubkey::from_str(&ctx.payload.account) {
             Ok(account_pubkey) => account_pubkey,
             _ => return Err(Error::InvalidAccountPubkey),
@@ -101,10 +140,19 @@ pub mod my_actions {
 #[action(
     icon = "https://google.com",
     title = "Fixed transfer",
-    description = "Send a fixed transfer fee to the treasury",
+    description = "Send a fixed transfer to the treasury",
     label = "Send"
 )]
 pub struct FixedTransferAction;
+
+#[derive(Action)]
+#[action(
+    icon = "https://google.com",
+    title = "Dynamic transfer",
+    description = "Send a dynamic transfer to the treasury",
+    label = "Send"
+)]
+pub struct DynamicTransferAction;
 
 #[derive(Debug, Serialize)]
 pub enum Error {
@@ -119,7 +167,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn it_handles_fixed_transfer_action() {
         let fixed_transfer_action_metadata = FixedTransferAction::to_metadata();
 
         assert_eq!("Fixed transfer", fixed_transfer_action_metadata.title);
@@ -131,6 +179,21 @@ mod tests {
             .unwrap();
 
         assert_eq!("AQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAQEEMlnIyV1k2VNRqM4x48htBRRy5jUZ2umQgMwoQ53uf4q5cX+QxKq3dF2j8lUSI+G9tMrUBw/nxQWe4oaNVv7qhPxCeH+W3dRh/wUfr48nA/12tCHT4rv2+H/cXKS0IZgdBt324ddloZPZy+FGzut5rBy0he1fWzeROoz1hX7/AKkAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEDBAECAAAJAwEAAAAAAAAA", fixed_transfer_action_transaction.transaction);
+    }
+
+    #[test]
+    fn it_handles_dynamic_transfer_action() {
+        let dynamic_transfer_action_metadata = DynamicTransferAction::to_metadata();
+
+        assert_eq!("Dynamic transfer", dynamic_transfer_action_metadata.title);
+
+        let dynamic_transfer_action_transaction =
+            DynamicTransferAction::handle_post_action(Json::from(CreateActionPayload {
+                account: "4PYnraBJbdPXeMXdgL5k1m3TCcfNMaEWycvEQu2cteEV".to_string(),
+            }))
+            .unwrap();
+
+        assert_eq!("AQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAQEEMlnIyV1k2VNRqM4x48htBRRy5jUZ2umQgMwoQ53uf4q5cX+QxKq3dF2j8lUSI+G9tMrUBw/nxQWe4oaNVv7qhPxCeH+W3dRh/wUfr48nA/12tCHT4rv2+H/cXKS0IZgdBt324ddloZPZy+FGzut5rBy0he1fWzeROoz1hX7/AKkAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEDBAECAAAJAwEAAAAAAAAA", dynamic_transfer_action_transaction.transaction);
     }
 }
 // END TESTING
