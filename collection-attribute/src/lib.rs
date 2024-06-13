ab0e48fbe135cdce0aa0cb3ffdd698d42571f14b
Dan <danielarturomt@gmail.com>
2024-06-13 21:49:16 +0100

    refactor: some house-keeping and cleaning things up

--- collection-attribute/src/lib.rs
+++ collection-attribute/src/lib.rs
@@ -1,106 +1,125 @@
 use proc_macro2::Ident;
-use syn::{FnArg, GenericArgument, Item, ItemMod, PathArguments, Type};
+use syn::{Block, FnArg, GenericArgument, Item, ItemMod, PathArguments, Type};
 
-fn collection_attribute_macro2(
-    input: proc_macro::TokenStream,
-) -> Result<proc_macro::TokenStream, syn::Error> {
-    // Parse the input tokens into a syntax tree
-    let input_module: ItemMod = syn::parse(input)?;
-
-    // Initialize an empty vector to hold function names
-    let mut functions = Vec::new();
-
-    // If the module has content, iterate over it to find function items
-    if let Some((_, items)) = &input_module.content {
-        for item in items {
-            if let Item::Fn(item_fn) = item {
-                functions.push(item_fn.clone());
+fn extract_action_ident(f: &syn::ItemFn) -> Option<&Ident> {
+    if let FnArg::Typed(pt) = f.sig.inputs.first()? {
+        if let Type::Path(type_path) = pt.ty.as_ref() {
+            if let PathArguments::AngleBracketed(inner_path) =
+                &type_path.path.segments.first()?.arguments
+            {
+                if let GenericArgument::Type(inner_type) = inner_path.args.first()? {
+                    if let Type::Path(inner_type_path) = inner_type {
+                        return inner_type_path.path.segments.first().map(|seg| &seg.ident);
+                    }
+                }
             }
         }
     }
-
-    // Generate implementation for create_transaction for the actions
-    let trait_impls = generate_trait_impl(&functions);
-
-    // TODO: Generate Routes and main
-
-    // TODO: Remove original function on behalf of generated one
-
-    // Combine the original module with the generated trait implementation
-    let result = quote::quote! {
-        #input_module
-        #trait_impls
-    };
-
-    Ok(result.into())
+    None
 }
 
-fn extract_action_ident(f: &syn::ItemFn) -> Option<Ident> {
-    let arg = f.sig.inputs.first()?;
-
-    let pattern_type = match arg {
-        FnArg::Typed(pt) => pt,
-        _ => return None,
-    };
-
-    let type_path = match pattern_type.ty.as_ref() {
-        Type::Path(tp) => tp,
-        _ => return None,
-    };
-
-    let inner_path = match &type_path.path.segments.first()?.arguments {
-        PathArguments::AngleBracketed(ip) => ip,
-        _ => return None,
-    };
-
-    let inner_type = match inner_path.args.first()? {
-        GenericArgument::Type(it) => it,
-        _ => return None,
-    };
-
-    let inner_type_path = match inner_type {
-        Type::Path(itp) => itp,
-        _ => return None,
-    };
-
-    Some(inner_type_path.path.segments.first()?.ident.clone())
-}
-
-fn extract_action_query(f: &syn::ItemFn) -> Option<Ident> {
-    let arg = f.sig.inputs.first().unwrap();
-
-    let pattern_type = match arg {
-        FnArg::Typed(pt) => pt,
-        _ => return None,
-    };
-
-    let type_path = match pattern_type.ty.as_ref() {
-        Type::Path(tp) => tp,
-        _ => return None,
-    };
-
-    if type_path.path.segments.first().unwrap().ident.to_string() != "ContextWithQuery" {
-        return None
+fn extract_action_query(f: &syn::ItemFn) -> Option<&Ident> {
+    if let FnArg::Typed(pt) = f.sig.inputs.first()? {
+        if let Type::Path(type_path) = pt.ty.as_ref() {
+            if type_path.path.segments.first()?.ident == "ContextWithQuery" {
+                if let PathArguments::AngleBracketed(inner_path) =
+                    &type_path.path.segments.first()?.arguments
+                {
+                    if let GenericArgument::Type(inner_type) = inner_path.args.last()? {
+                        if let Type::Path(inner_type_path) = inner_type {
+                            return inner_type_path.path.segments.first().map(|seg| &seg.ident);
+                        }
+                    }
+                }
+            }
+        }
     }
+    None
+}
 
-    let inner_path = match &type_path.path.segments.first()?.arguments {
-        PathArguments::AngleBracketed(ip) => ip,
-        _ => return None,
-    };
+fn generate_impls_with_query(
+    action_ident: &Ident,
+    fn_block: &Box<Block>,
+    action_query_type_ident: &Ident,
+) -> (proc_macro2::TokenStream, proc_macro2::TokenStream) {
+    (
+        quote::quote! {
+            impl CreateTransactionWithQuery<#action_ident, #action_query_type_ident> for #action_ident {
+                fn create_transaction(&self, ctx: ContextWithQuery<#action_ident, #action_query_type_ident>) -> Result<solana_sdk::transaction::Transaction, Error> {
+                    #fn_block
+                }
+            }
+        },
+        quote::quote! {
+            impl HandlePostActionWithQuery<#action_query_type_ident> for #action_ident {
+                fn handle_post_action(
+                    axum::Json(payload): axum::Json<CreateActionPayload>,
+                    axum::extract::Query(query): axum::extract::Query<#action_query_type_ident>
+                ) -> Result<axum::Json<ActionTransaction>, Error> {
+                    let action = #action_ident;
+                    let context_with_query = ContextWithQuery::<#action_ident, #action_query_type_ident> {
+                        payload,
+                        action: PhantomData,
+                        query
+                    };
+                    let transaction = action.create_transaction(context_with_query).unwrap();
+                    let serialized_transaction = bincode::serialize(&transaction).unwrap();
+                    let encoded_transaction = BASE64_STANDARD.encode(serialized_transaction);
 
-    let inner_type = match inner_path.args.last()? {
-        GenericArgument::Type(it) => it,
-        _ => return None,
-    };
+                    Ok(axum::Json(ActionTransaction {
+                        transaction: encoded_transaction,
+                        message: None
+                    }))
+                }
+            }
+        },
+    )
+}
 
-    let inner_type_path = match inner_type {
-        Type::Path(itp) => itp,
-        _ => return None,
-    };
+fn generate_impls_without_query(
+    action_ident: &Ident,
+    fn_block: &Box<Block>,
+) -> (proc_macro2::TokenStream, proc_macro2::TokenStream) {
+    (
+        quote::quote! {
+            impl CreateTransaction<#action_ident> for #action_ident {
+                fn create_transaction(&self, ctx: Context<#action_ident>) -> Result<solana_sdk::transaction::Transaction, Error> {
+                    #fn_block
+                }
+            }
+        },
+        quote::quote! {
+            impl HandlePostAction for #action_ident {
+                fn handle_post_action(axum::Json(payload): axum::Json<CreateActionPayload>) -> Result<axum::Json<ActionTransaction>, Error> {
+                    let action = #action_ident {};
+                    let context = Context::<#action_ident> {
+                        payload,
+                        action: PhantomData,
+                    };
+                    let transaction = action.create_transaction(context).unwrap();
+                    let serialized_transaction = bincode::serialize(&transaction).unwrap();
+                    let encoded_transaction = BASE64_STANDARD.encode(serialized_transaction);
 
-    let query_type_ident = inner_type_path.path.segments.first().unwrap();
+                    Ok(axum::Json(ActionTransaction {
+                        transaction: encoded_transaction,
+                        message: None
+                    }))
+                }
+            }
+        },
+    )
+}
 
-    Some(query_type_ident.ident.clone())
+fn generate_handle_get_action_impl(action_ident: &Ident) -> proc_macro2::TokenStream {
+    quote::quote! {
+        impl HandleGetAction for #action_ident {
+            fn handle_get_action() -> Result<axum::Json<ActionMetadata>, Error> {
+                let action = #action_ident;
+
+                Ok(axum::Json(action.to_metadata()))
+            }
+        }
+    }
 }
 
 fn generate_trait_impl(functions: &[syn::ItemFn]) -> proc_macro2::TokenStream {
@@ -109,79 +128,14 @@ fn generate_trait_impl(functions: &[syn::ItemFn]) -> proc_macro2::TokenStream {
         .map(|f| {
             let action_ident = extract_action_ident(&f).unwrap();
             let fn_block = &f.block;
-            let handle_get_action_impl = quote::quote! {
-                impl HandleGetAction for #action_ident {
-                    fn handle_get_action() -> Result<axum::Json<ActionMetadata>, Error> {
-                        let action = #action_ident;
-        
-                        Ok(axum::Json(action.to_metadata()))
-                    }
-                }
-            };
-            let create_transaction_impl: proc_macro2::TokenStream;
-            let handle_post_action_impl: proc_macro2::TokenStream;
+            let handle_get_action_impl = generate_handle_get_action_impl(action_ident);
 
-            if let Some(action_query_type_ident) = extract_action_query(&f) {
-                create_transaction_impl = quote::quote! {
-                    impl CreateTransactionWithQuery<#action_ident, #action_query_type_ident> for #action_ident {
-                        fn create_transaction(&self, ctx: ContextWithQuery<#action_ident, #action_query_type_ident>) -> Result<solana_sdk::transaction::Transaction, Error> {
-                            #fn_block
-                        }
-                    }
+            let (create_transaction_impl, handle_post_action_impl) =
+                if let Some(action_query_type_ident) = extract_action_query(&f) {
+                    generate_impls_with_query(action_ident, fn_block, action_query_type_ident)
+                } else {
+                    generate_impls_without_query(action_ident, fn_block)
                 };
-
-                handle_post_action_impl = quote::quote! {
-                    impl HandlePostActionWithQuery<#action_query_type_ident> for #action_ident {
-                        fn handle_post_action(
-                            axum::Json(payload): axum::Json<CreateActionPayload>,
-                            axum::extract::Query(query): axum::extract::Query<#action_query_type_ident>
-                        ) -> Result<axum::Json<ActionTransaction>, Error> {
-                            let action = #action_ident;
-                            let context_with_query = ContextWithQuery::<#action_ident, #action_query_type_ident> {
-                                payload,
-                                action: PhantomData,
-                                query
-                            };
-                            let transaction = action.create_transaction(context_with_query).unwrap();
-                            let serialized_transaction = bincode::serialize(&transaction).unwrap();
-                            let encoded_transaction = BASE64_STANDARD.encode(serialized_transaction);
-                    
-                            Ok(axum::Json(ActionTransaction {
-                                transaction: encoded_transaction,
-                                message: None
-                            }))
-                        }
-                    }
-                };
-            } else {
-                create_transaction_impl = quote::quote! {
-                    impl CreateTransaction<#action_ident> for #action_ident {
-                        fn create_transaction(&self, ctx: Context<#action_ident>) -> Result<solana_sdk::transaction::Transaction, Error> {
-                            #fn_block
-                        }
-                    }
-                };
-
-                handle_post_action_impl = quote::quote! {
-                    impl HandlePostAction for #action_ident {
-                        fn handle_post_action(axum::Json(payload): axum::Json<CreateActionPayload>) -> Result<axum::Json<ActionTransaction>, Error> {
-                            let action = #action_ident {};
-                            let context = Context::<#action_ident> {
-                                payload,
-                                action: PhantomData,
-                            };
-                            let transaction = action.create_transaction(context).unwrap();
-                            let serialized_transaction = bincode::serialize(&transaction).unwrap();
-                            let encoded_transaction = BASE64_STANDARD.encode(serialized_transaction);
-        
-                            Ok(axum::Json(ActionTransaction {
-                                transaction: encoded_transaction,
-                                message: None
-                            }))
-                        }
-                    }
-                };
-            }
 
             quote::quote! {
                 #create_transaction_impl
@@ -201,5 +155,29 @@ pub fn collection(
     _args: proc_macro::TokenStream,
     input: proc_macro::TokenStream,
 ) -> proc_macro::TokenStream {
-    collection_attribute_macro2(input.into()).unwrap().into()
+    // Parse the input tokens into a syntax tree
+    let input_module: ItemMod = syn::parse(input).unwrap();
+
+    // Initialize an empty vector to hold function names
+    let mut functions = Vec::new();
+
+    // If the module has content, iterate over it to find function items
+    if let Some((_, items)) = &input_module.content {
+        for item in items {
+            if let Item::Fn(item_fn) = item {
+                functions.push(item_fn.clone());
+            }
+        }
+    }
+
+    // Generate implementation for create_transaction for the actions
+    let trait_impls = generate_trait_impl(&functions);
+
+    // Combine the original module with the generated trait implementation
+    let result = quote::quote! {
+        #input_module
+        #trait_impls
+    };
+
+    result.into()
 }
