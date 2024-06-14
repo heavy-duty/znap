104198b539628da31a017fcece036c56d89a86ab
Dan <danielarturomt@gmail.com>
2024-06-14 23:31:58 +0100

    chore: slightly house keeping

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use axum::Router;
 use my_actions::collection_router;
 use tokio::net::TcpListener;
 use znap_lang::*;
@@ -5,9 +6,10 @@ use znap_lang::*;
 #[tokio::main]
 async fn main() -> Result<(), ActionError> {
     let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
-    
+    let router = Router::new().merge(collection_router());
+
     println!("->> LISTENING on {:?}\n", listener.local_addr());
-    axum::serve(listener, collection_router().into_make_service())
+    axum::serve(listener, router.into_make_service())
         .await
         .unwrap();
 
