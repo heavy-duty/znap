afc85c67b06498a937ebab33fe315a11136fda44
Dan <danielarturomt@gmail.com>
2024-06-14 23:17:23 +0100

    chore: house keeping

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ use znap_lang::*;
 async fn main() -> Result<(), ActionError> {
     let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
     
-    println!("{:?}",collection_router());
     println!("->> LISTENING on {:?}\n", listener.local_addr());
     axum::serve(listener, collection_router().into_make_service())
         .await
