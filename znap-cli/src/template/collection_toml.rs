a86f0eaa896b26b50644c974885544096085ebfc
Daniel Marin <danielarturomt@gmail.com>
2024-06-18 11:32:56 +0100

    refactor: re-organize the entire workspace (#9)
    * refactor: re-organize the entire workspace
    
    * chore: bump crate versions
    
    * chore: add derive feature to serde
    
    * chore: remove vscode config
    
    * chore: add vscode to gitignore

---
+++ znap-cli/src/template/collection_toml.rs
@@ -0,0 +1,17 @@
+use heck::ToKebabCase;
+
+pub fn template(name: &String) -> String {
+    format!("[package]\n\
+        name = \"{}\"\n\
+        version = \"0.1.0\"\n\
+        edition = \"2021\"\n\
+        \n[dependencies]\n\
+        axum = \"0.7.5\"\n\
+        serde = \"1.0.203\"\n\
+        solana-sdk = \"1.18.16\"\n\
+        spl-associated-token-account = \"3.0.2\"\n\
+        spl-token = \"4.0.1\"\n\
+        znap = \"0.1.2\"\n\
+    ", name.to_kebab_case())
+}
+
---
+++ znap-macros/src/lib.rs
@@ -0,0 +1,32 @@
+use proc_macro::TokenStream;
+use syn::parse_macro_input;
+use znap_syn::{ActionStruct, CollectionMod, ErrorEnum, QueryStruct};
+use quote::ToTokens;
+
+#[proc_macro_derive(Action, attributes(action))]
+pub fn action_derive_macro(item: TokenStream) -> TokenStream {
+    parse_macro_input!(item as ActionStruct)
+        .to_token_stream()
+        .into()
+}
+
+#[proc_macro_derive(ErrorCode, attributes(error))]
+pub fn error_code_derive_macro(item: TokenStream) -> TokenStream {
+    parse_macro_input!(item as ErrorEnum)
+        .to_token_stream()
+        .into()
+}
+
+#[proc_macro_attribute]
+pub fn query(_args: TokenStream, input: TokenStream) -> TokenStream {
+    parse_macro_input!(input as QueryStruct)
+        .to_token_stream()
+        .into()
+}
+
+#[proc_macro_attribute]
+pub fn collection(_args: TokenStream, input: TokenStream) -> TokenStream {
+    parse_macro_input!(input as CollectionMod)
+        .to_token_stream()
+        .into()
+}
