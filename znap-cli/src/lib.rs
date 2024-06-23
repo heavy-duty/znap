f4ca9c1dec9f07dc3028b71760ec160dbce1500f
Sergio Alejandro Ribera Costa <56278796+SergioRibera@users.noreply.github.com>
2024-06-23 07:50:47 -0400

    feat(cli): support change address and port on serve (#30)

--- znap-cli/src/lib.rs
+++ znap-cli/src/lib.rs
@@ -16,7 +16,12 @@ pub enum Command {
     /// Builds all collections from the workspace
     Build,
     /// Serves all collections from the workspace
-    Serve,
+    Serve {
+        #[clap(short, long, default_value = "127.0.0.1")]
+        address: String,
+        #[clap(short, long, default_value = "3000")]
+        port: u16,
+    },
     /// Runs the test suite for the workspace
     Test,
     /// Cleans all the temp files
@@ -32,16 +37,16 @@ pub enum Command {
     /// Create a new collection in the workspace
     New {
         name: String,
-         /// Skip writing the files.
-         #[clap(short, long)]
-         dry_run: bool,
+        /// Skip writing the files.
+        #[clap(short, long)]
+        dry_run: bool,
     },
 }
 
 fn process_command(opts: Opts) -> Result<()> {
     match &opts.command {
         Command::Build => Ok(commands::build::run()),
-        Command::Serve => Ok(commands::serve::run()),
+        Command::Serve { address, port } => Ok(commands::serve::run(&address, *port)),
         Command::Test => Ok(commands::test::run()),
         Command::Clean => Ok(commands::clean::run()),
         Command::Init { name, dry_run } => Ok(commands::init::run(&name, &dry_run)),
@@ -49,7 +54,7 @@ fn process_command(opts: Opts) -> Result<()> {
     }
 }
 
-pub fn entry(opts: Opts) -> Result<()>  {
-
+pub fn entry(opts: Opts) -> Result<()> {
     process_command(opts)
 }
+
