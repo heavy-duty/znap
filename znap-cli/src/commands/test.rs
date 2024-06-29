66fde2f01c906967e14e886c3b6e5074a8dab7e3
Dan <danielarturomt@gmail.com>
2024-06-29 18:15:54 +0100

    feat: test command from cli now works

--- znap-cli/src/commands/test.rs
+++ znap-cli/src/commands/test.rs
@@ -1,3 +1,33 @@
+use anyhow::anyhow;
+use std::process::Stdio;
+
 pub fn run() {
-    println!("TODO: Testing your workspace");
+    let mut serve_handle = std::process::Command::new("znap")
+        .arg("serve")
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .spawn()
+        .map_err(|e| anyhow!("Failed to spawn `znap serve`: {e}"))
+        .unwrap();
+
+    std::thread::sleep(std::time::Duration::from_millis(5000));
+
+    std::process::Command::new("npm")
+        .arg("run")
+        .arg("test")
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .output()
+        .map_err(anyhow::Error::from)
+        .unwrap();
+
+    serve_handle.kill().unwrap();
+
+    std::process::Command::new("znap")
+        .arg("clean")
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .output()
+        .map_err(anyhow::Error::from)
+        .unwrap();
 }
