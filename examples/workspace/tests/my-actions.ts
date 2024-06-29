d5f2b6af6347d1c96ffcdafc7947fa3e9f665795
Dan <danielarturomt@gmail.com>
2024-06-29 18:27:11 +0100

    refactor: create a client and move it to utils

--- examples/workspace/tests/my-actions.ts
+++ examples/workspace/tests/my-actions.ts
@@ -2,30 +2,15 @@ import {
   Keypair,
   LAMPORTS_PER_SOL,
   SystemInstruction,
-  Transaction
+  Transaction,
 } from "@solana/web3.js";
 import { assert } from "chai";
-
-interface Action {
-  label: string;
-  href: string;
-  parameters: {
-    label: string;
-    name: string;
-  }[];
-}
-
-interface Metadata {
-  icon: string;
-  title: string;
-  description: string;
-  label: string;
-  disabled: boolean;
-  error: null;
-  links: { actions: Action[] };
-}
+import { Metadata, createClient } from "./utils";
 
 describe("My actions", () => {
+  const baseUrl = "http://localhost:3000";
+  const znapClient = createClient(baseUrl);
+  const bobKeypair = Keypair.generate();
   const actionMetadata: Metadata = {
     icon: "https://media.discordapp.net/attachments/1205590693041541181/1212566609202520065/icon.png?ex=667eb568&is=667d63e8&hm=0f247078545828c0a5cf8300a5601c56bbc9b59d3d87a0c74b082df0f3a6d6bd&=&format=webp&quality=lossless&width=660&height=660",
     title: "Alice's website",
@@ -59,28 +44,17 @@ describe("My actions", () => {
     error: null,
   };
 
-  const baseUrl = "http://localhost:3000";
-
-  const bobKeypair = Keypair.generate();
-
   it("should fetch the metadata of the send donation action", async () => {
-    const url = new URL(`${baseUrl}/api/send_donation`);
-    const response = await fetch(url.toString(), {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    const responseJson = (await response.json()) as Metadata;
+    const response = await znapClient.getMetadata("send_donation");
 
-    assert.equal(responseJson.title, actionMetadata.title);
-    assert.equal(responseJson.description, actionMetadata.description);
-    assert.equal(responseJson.icon, actionMetadata.icon);
-    assert.equal(responseJson.label, actionMetadata.label);
-    assert.equal(responseJson.disabled, actionMetadata.disabled);
-    assert.equal(responseJson.error, actionMetadata.error);
+    assert.equal(response.title, actionMetadata.title);
+    assert.equal(response.description, actionMetadata.description);
+    assert.equal(response.icon, actionMetadata.icon);
+    assert.equal(response.label, actionMetadata.label);
+    assert.equal(response.disabled, actionMetadata.disabled);
+    assert.equal(response.error, actionMetadata.error);
 
-    responseJson.links.actions.forEach((link, linkIndex) => {
+    response.links.actions.forEach((link, linkIndex) => {
       assert.equal(link.href, actionMetadata.links.actions[linkIndex].href);
       assert.equal(link.label, actionMetadata.links.actions[linkIndex].label);
 
@@ -103,21 +77,13 @@ describe("My actions", () => {
 
   it("should create a valid send donation transaction", async () => {
     const amount = 5_000;
-    const url = new URL(`${baseUrl}/api/send_donation`);
-
-    url.searchParams.set("amount", amount.toString());
-
-    const response = await fetch(url.toString(), {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ account: bobKeypair.publicKey.toBase58() }),
-    });
-    const responseJson = (await response.json()) as { transaction: string };
-
+    const response = await znapClient.getTransaction(
+      "send_donation",
+      bobKeypair.publicKey.toBase58(),
+      { amount }
+    );
     const transaction = Transaction.from(
-      Buffer.from(responseJson.transaction, "base64")
+      Buffer.from(response.transaction, "base64")
     );
     const transferInstruction = SystemInstruction.decodeTransfer(
       transaction.instructions[0]
