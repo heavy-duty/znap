d5fe8d89ecf47caec311dcfea6f4112ac991ee72
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2024-07-17 00:05:31 -0400

    fix: action name generation function

--- znap-syn/src/codegen/action/path.rs
+++ znap-syn/src/codegen/action/path.rs
@@ -2,14 +2,12 @@ use crate::{
     common::{action_name_without_suffix, create_path},
     ActionStruct,
 };
-use heck::ToSnekCase;
 use proc_macro2::TokenStream;
 use quote::quote;
 
 pub fn generate(action_struct: &ActionStruct) -> TokenStream {
     let path = create_path(&action_struct.name.to_string());
-    let action_name =
-        action_name_without_suffix(&action_struct.name.to_string().to_snek_case()).to_snek_case();
+    let action_name = action_name_without_suffix(&action_struct.name.to_string());
 
     if let Some(params_attrs) = &action_struct.params_attrs {
         let mut segments: Vec<String> = vec!["/api".to_string(), action_name];
