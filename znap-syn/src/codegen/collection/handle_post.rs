845ea167945f08008755730546435154d7fccd9a
Dan <danielarturomt@gmail.com>
2024-08-01 21:07:53 +0100

    feat: add blockhash to the transaction

--- znap-syn/src/codegen/collection/handle_post.rs
+++ znap-syn/src/codegen/collection/handle_post.rs
@@ -45,7 +45,15 @@ pub fn generate(collection_mod: &CollectionMod) -> TokenStream {
                         env: znap::env::Env::default(),
                     };
                     let znap::ActionTransaction { transaction, message } = #create_transaction_fn(&context).await?;
-                    let transaction_with_identity = znap::add_action_identity_proof(transaction, &context.env.keypair);
+                    let mut transaction_with_identity = znap::add_action_identity_proof(transaction, &context.env.keypair);
+                    let rpc_client = solana_client::rpc_client::RpcClient::new_with_commitment(
+                        "https://api.devnet.solana.com".to_string(),
+                        solana_sdk::commitment_config::CommitmentConfig::confirmed(),
+                    );
+                    let recent_blockhash = rpc_client
+                        .get_latest_blockhash()
+                        .or_else(|_| Err(Error::from(ActionError::InvalidReceiverPublicKey)))?;
+                    transaction_with_identity.message.recent_blockhash = recent_blockhash;
                     let serialized_transaction = bincode::serialize(&transaction_with_identity).unwrap();
                     let encoded_transaction = BASE64_STANDARD.encode(serialized_transaction);
 
