d43ba65cf8141b34c7dc230ada58264aeade96db
Dan <danielarturomt@gmail.com>
2024-08-29 20:43:25 +0100

    test: add test case for default path and multiple parameters

--- tests/paths/collections/paths/src/lib.rs
+++ tests/paths/collections/paths/src/lib.rs
@@ -9,21 +9,20 @@ use znap::prelude::*;
 pub mod paths {
     use super::*;
 
-    pub fn paths(ctx: Context<PathsAction>) -> Result<ActionTransaction> {
-        let account_pubkey = Pubkey::from_str(&ctx.payload.account)
-            .or_else(|_| Err(Error::from(ActionError::InvalidAccountPublicKey)))?;
-        let receiver_pubkey = Pubkey::from_str(&ctx.params.receiver_address)
-            .or_else(|_| Err(Error::from(ActionError::InvalidReceiverPublicKey)))?;
-        let transfer_instruction = transfer(
-            &account_pubkey,
-            &receiver_pubkey,
-            ctx.query.amount * LAMPORTS_PER_SOL,
-        );
-        let transaction_message = Message::new(&[transfer_instruction], None);
-        let transaction = Transaction::new_unsigned(transaction_message);
-
+    pub fn custom_path_single_parameter(
+        ctx: Context<CustomPathSingleParameterAction>,
+    ) -> Result<ActionTransaction> {
         Ok(ActionTransaction {
-            transaction,
+            transaction: Transaction::new_unsigned(Message::new(&[], None)),
+            message: Some("send donation to alice".to_string()),
+        })
+    }
+
+    pub fn default_path_multi_parameter(
+        ctx: Context<DefaultPathMultiParameterAction>,
+    ) -> Result<ActionTransaction> {
+        Ok(ActionTransaction {
+            transaction: Transaction::new_unsigned(Message::new(&[], None)),
             message: Some("send donation to alice".to_string()),
         })
     }
@@ -32,32 +31,22 @@ pub mod paths {
 #[derive(Action)]
 #[action(
     icon = "https://media.discordapp.net/attachments/1205590693041541181/1212566609202520065/icon.png?ex=667eb568&is=667d63e8&hm=0f247078545828c0a5cf8300a5601c56bbc9b59d3d87a0c74b082df0f3a6d6bd&=&format=webp&quality=lossless&width=660&height=660",
-    title = "Send a Donation to {{params.receiver_address}}",
-    description = "Send a donation to {{params.receiver_address}} using the Solana blockchain via a Blink.",
+    title = "Custom Path with Single Paramater",
+    description = "Use a custom path configuration with a single parameter",
     label = "Send",
-    path = "{{prefix}}/v1/test/{{action_name}}",
-    link = {
-        label = "Send 1 SOL",
-        href = "?amount=1",
-    },
-    link = {
-        label = "Send 5 SOL",
-        href = "?amount=5",
-    },
-    link = {
-        label = "Send SOL",
-        href = "http://localhost:3000/api/send_donation?amount={amount}",
-        parameter = { label = "Amount in SOL", name = "amount" }
-    },
+    path = "{{prefix}}/v1/test/{{action_name}}"
 )]
 #[query(amount: u64)]
 #[params(receiver_address: String)]
-pub struct PathsAction;
+pub struct CustomPathSingleParameterAction;
 
-#[derive(ErrorCode)]
-enum ActionError {
-    #[error(msg = "Invalid account public key")]
-    InvalidAccountPublicKey,
-    #[error(msg = "Invalid receiver public key")]
-    InvalidReceiverPublicKey,
-}
+#[derive(Action)]
+#[action(
+    icon = "https://media.discordapp.net/attachments/1205590693041541181/1212566609202520065/icon.png?ex=667eb568&is=667d63e8&hm=0f247078545828c0a5cf8300a5601c56bbc9b59d3d87a0c74b082df0f3a6d6bd&=&format=webp&quality=lossless&width=660&height=660",
+    title = "Default Path with Multiple Paramaters",
+    description = "Use the default path configuration with multiple parameters",
+    label = "Send"
+)]
+#[query(amount: u64)]
+#[params(mint_address: String, receiver_address: String)]
+pub struct DefaultPathMultiParameterAction;
