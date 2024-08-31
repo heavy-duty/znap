b28a0a46dacdb20133e95aafb0154362932d2420
Dan <danielarturomt@gmail.com>
2024-08-31 15:01:38 +0100

    test: add test cases for path with dynamic metadata

--- tests/paths/collections/paths/src/lib.rs
+++ tests/paths/collections/paths/src/lib.rs
@@ -8,14 +8,70 @@ pub mod paths {
     pub fn custom_path(ctx: Context<CustomPathAction>) -> Result<ActionTransaction> {
         Ok(ActionTransaction {
             transaction: Transaction::new_unsigned(Message::new(&[], None)),
-            message: Some("send donation to alice".to_string()),
+            message: Some("action with custom path".to_string()),
         })
     }
 
     pub fn default_path(ctx: Context<DefaultPathAction>) -> Result<ActionTransaction> {
         Ok(ActionTransaction {
             transaction: Transaction::new_unsigned(Message::new(&[], None)),
-            message: Some("send donation to alice".to_string()),
+            message: Some("action with default path".to_string()),
+        })
+    }
+
+    pub fn custom_path_with_dynamic_metadata_action_get(
+        ctx: Context<CustomPathWithDynamicMetadataAction>,
+    ) -> Result<ActionMetadata> {
+        let label = "Send";
+        let description = "Use a custom path configuration with dynamic metadata";
+        let title = "Custom Path with Dynamic Metadata";
+        let icon = "https://media.discordapp.net/attachments/1205590693041541181/1212566609202520065/icon.png?ex=667eb568&is=667d63e8&hm=0f247078545828c0a5cf8300a5601c56bbc9b59d3d87a0c74b082df0f3a6d6bd&=&format=webp&quality=lossless&width=660&height=660";
+
+        Ok(ActionMetadata {
+            title: title.to_string(),
+            description: description.to_string(),
+            icon: icon.to_string(),
+            label: label.to_string(),
+            disabled: false,
+            error: None,
+            links: None,
+        })
+    }
+
+    pub fn custom_path_with_dynamic_metadata_action_post(
+        ctx: Context<CustomPathWithDynamicMetadataAction>,
+    ) -> Result<ActionTransaction> {
+        Ok(ActionTransaction {
+            transaction: Transaction::new_unsigned(Message::new(&[], None)),
+            message: Some("action with custom path and dynamic metadata".to_string()),
+        })
+    }
+
+    pub fn default_path_with_dynamic_metadata_action_get(
+        ctx: Context<DefaultPathWithDynamicMetadataAction>,
+    ) -> Result<ActionMetadata> {
+        let label = "Send";
+        let description = "Use a default path configuration with dynamic metadata";
+        let title = "Default Path with Dynamic Metadata";
+        let icon = "https://media.discordapp.net/attachments/1205590693041541181/1212566609202520065/icon.png?ex=667eb568&is=667d63e8&hm=0f247078545828c0a5cf8300a5601c56bbc9b59d3d87a0c74b082df0f3a6d6bd&=&format=webp&quality=lossless&width=660&height=660";
+
+        Ok(ActionMetadata {
+            title: title.to_string(),
+            description: description.to_string(),
+            icon: icon.to_string(),
+            label: label.to_string(),
+            disabled: false,
+            error: None,
+            links: None,
+        })
+    }
+
+    pub fn default_path_with_dynamic_metadata_action_post(
+        ctx: Context<DefaultPathWithDynamicMetadataAction>,
+    ) -> Result<ActionTransaction> {
+        Ok(ActionTransaction {
+            transaction: Transaction::new_unsigned(Message::new(&[], None)),
+            message: Some("action with default path and dynamic metadata".to_string()),
         })
     }
 }
@@ -42,3 +98,12 @@ pub struct CustomPathAction;
 #[query(amount: u64)]
 #[params(receiver_address: String)]
 pub struct DefaultPathAction;
+
+#[derive(Action)]
+#[query(amount: u64)]
+#[action_path(template = "{{prefix}}/super_custom/{{action_name}}")]
+pub struct CustomPathWithDynamicMetadataAction;
+
+#[derive(Action)]
+#[query(amount: u64)]
+pub struct DefaultPathWithDynamicMetadataAction;
--- tests/paths/tests/paths.ts
+++ tests/paths/tests/paths.ts
@@ -5,11 +5,10 @@ import { Metadata, createActionClient } from "./utils";
 describe("Paths Tests", () => {
   const baseUrl = "http://localhost:3001";
   const aliceKeypair = Keypair.generate();
-  const mintKeypair = Keypair.generate();
-  const customPathSingleParameterClient = createActionClient(
+  const customPathClient = createActionClient(
     `${baseUrl}/api/v1/test/custom_path/${aliceKeypair.publicKey.toBase58()}`
   );
-  const customPathSingleParameterMetadata: Metadata = {
+  const customPathMetadata: Metadata = {
     icon: "https://media.discordapp.net/attachments/1205590693041541181/1212566609202520065/icon.png?ex=667eb568&is=667d63e8&hm=0f247078545828c0a5cf8300a5601c56bbc9b59d3d87a0c74b082df0f3a6d6bd&=&format=webp&quality=lossless&width=660&height=660",
     title: "Custom Path",
     description: "Use a custom path configuration",
@@ -18,10 +17,10 @@ describe("Paths Tests", () => {
     disabled: false,
     error: null,
   };
-  const defaultPathMultiParameterClient = createActionClient(
+  const defaultPathClient = createActionClient(
     `${baseUrl}/api/default_path/${aliceKeypair.publicKey.toBase58()}`
   );
-  const defaultPathMultiParameterMetadata: Metadata = {
+  const defaultPathMetadata: Metadata = {
     icon: "https://media.discordapp.net/attachments/1205590693041541181/1212566609202520065/icon.png?ex=667eb568&is=667d63e8&hm=0f247078545828c0a5cf8300a5601c56bbc9b59d3d87a0c74b082df0f3a6d6bd&=&format=webp&quality=lossless&width=660&height=660",
     title: "Default Path",
     description: "Use the default path configuration",
@@ -30,34 +29,88 @@ describe("Paths Tests", () => {
     disabled: false,
     error: null,
   };
+  const customPathWithDynamicMetadataClient = createActionClient(
+    `${baseUrl}/api/super_custom/custom_path_with_dynamic_metadata`
+  );
+  const customPathWithDynamicMetadataMetadata: Metadata = {
+    icon: "https://media.discordapp.net/attachments/1205590693041541181/1212566609202520065/icon.png?ex=667eb568&is=667d63e8&hm=0f247078545828c0a5cf8300a5601c56bbc9b59d3d87a0c74b082df0f3a6d6bd&=&format=webp&quality=lossless&width=660&height=660",
+    title: "Custom Path with Dynamic Metadata",
+    description: "Use a custom path configuration with dynamic metadata",
+    label: "Send",
+    links: null,
+    disabled: false,
+    error: null,
+  };
+  const defaultPathWithDynamicMetadataClient = createActionClient(
+    `${baseUrl}/api/default_path_with_dynamic_metadata`
+  );
+  const defaultPathWithDynamicMetadataMetadata: Metadata = {
+    icon: "https://media.discordapp.net/attachments/1205590693041541181/1212566609202520065/icon.png?ex=667eb568&is=667d63e8&hm=0f247078545828c0a5cf8300a5601c56bbc9b59d3d87a0c74b082df0f3a6d6bd&=&format=webp&quality=lossless&width=660&height=660",
+    title: "Default Path with Dynamic Metadata",
+    description: "Use a default path configuration with dynamic metadata",
+    label: "Send",
+    links: null,
+    disabled: false,
+    error: null,
+  };
 
   it("should fetch the metadata of the custom path action", async () => {
-    const response = await customPathSingleParameterClient.getMetadata();
+    const response = await customPathClient.getMetadata();
 
-    assert.equal(response.title, customPathSingleParameterMetadata.title);
-    assert.equal(
-      response.description,
-      customPathSingleParameterMetadata.description
-    );
-    assert.equal(response.icon, customPathSingleParameterMetadata.icon);
-    assert.equal(response.label, customPathSingleParameterMetadata.label);
-    assert.equal(response.disabled, customPathSingleParameterMetadata.disabled);
-    assert.equal(response.error, customPathSingleParameterMetadata.error);
-    assert.equal(response.links, customPathSingleParameterMetadata.links);
+    assert.equal(response.title, customPathMetadata.title);
+    assert.equal(response.description, customPathMetadata.description);
+    assert.equal(response.icon, customPathMetadata.icon);
+    assert.equal(response.label, customPathMetadata.label);
+    assert.equal(response.disabled, customPathMetadata.disabled);
+    assert.equal(response.error, customPathMetadata.error);
+    assert.equal(response.links, customPathMetadata.links);
   });
 
   it("should fetch the metadata of the default path action", async () => {
-    const response = await defaultPathMultiParameterClient.getMetadata();
+    const response = await defaultPathClient.getMetadata();
 
-    assert.equal(response.title, defaultPathMultiParameterMetadata.title);
+    assert.equal(response.title, defaultPathMetadata.title);
+    assert.equal(response.description, defaultPathMetadata.description);
+    assert.equal(response.icon, defaultPathMetadata.icon);
+    assert.equal(response.label, defaultPathMetadata.label);
+    assert.equal(response.disabled, defaultPathMetadata.disabled);
+    assert.equal(response.error, defaultPathMetadata.error);
+    assert.equal(response.links, defaultPathMetadata.links);
+  });
+
+  it("should fetch the metadata of the custom path with dynamic metadata action", async () => {
+    const response = await customPathWithDynamicMetadataClient.getMetadata();
+
+    assert.equal(response.title, customPathWithDynamicMetadataMetadata.title);
     assert.equal(
       response.description,
-      defaultPathMultiParameterMetadata.description
+      customPathWithDynamicMetadataMetadata.description
     );
-    assert.equal(response.icon, defaultPathMultiParameterMetadata.icon);
-    assert.equal(response.label, defaultPathMultiParameterMetadata.label);
-    assert.equal(response.disabled, defaultPathMultiParameterMetadata.disabled);
-    assert.equal(response.error, defaultPathMultiParameterMetadata.error);
-    assert.equal(response.links, defaultPathMultiParameterMetadata.links);
+    assert.equal(response.icon, customPathWithDynamicMetadataMetadata.icon);
+    assert.equal(response.label, customPathWithDynamicMetadataMetadata.label);
+    assert.equal(
+      response.disabled,
+      customPathWithDynamicMetadataMetadata.disabled
+    );
+    assert.equal(response.error, customPathWithDynamicMetadataMetadata.error);
+    assert.equal(response.links, customPathWithDynamicMetadataMetadata.links);
+  });
+
+  it("should fetch the metadata of the default path with dynamic metadata action", async () => {
+    const response = await defaultPathWithDynamicMetadataClient.getMetadata();
+
+    assert.equal(response.title, defaultPathWithDynamicMetadataMetadata.title);
+    assert.equal(
+      response.description,
+      defaultPathWithDynamicMetadataMetadata.description
+    );
+    assert.equal(response.icon, defaultPathWithDynamicMetadataMetadata.icon);
+    assert.equal(response.label, defaultPathWithDynamicMetadataMetadata.label);
+    assert.equal(
+      response.disabled,
+      defaultPathWithDynamicMetadataMetadata.disabled
+    );
+    assert.equal(response.error, defaultPathWithDynamicMetadataMetadata.error);
+    assert.equal(response.links, defaultPathWithDynamicMetadataMetadata.links);
   });
 });
